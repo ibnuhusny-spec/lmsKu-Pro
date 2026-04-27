74f35e2b37a0c1a4215f61fcacde5eebd467b9f7
Ibnu Husny <ibnuhusny@gmail.com>
2026-04-27 15:05:19 +0800

    ubah firebase jadi template env

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider } from "firebase/auth"; // MODUL BARU
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+// Mesin ini akan otomatis mengambil data dari Brankas Vercel / File .env
 const firebaseConfig = {
-  apiKey: "AIzaSyAmEvT9DtYQSWG2THiMROGI5MJPxMHXJug",
-  authDomain: "lmsku-pro.firebaseapp.com",
-  projectId: "lmsku-pro",
-  storageBucket: "lmsku-pro.firebasestorage.app",
-  messagingSenderId: "1034895418174",
-  appId: "1:1034895418174:web:8f3ee99c1c0ec1c326c26f"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
-// MENGAKTIFKAN LOGIN GMAIL
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
+export { db, auth, googleProvider };
